8af08e5630b956b90d3a5624060ead760ab867be
Kankati Aneesh <120101251+anion12@users.noreply.github.com>
2025-02-19 22:38:54 +0530

    19-02-2025

---
+++ BuildArrayFromPermutation.java
@@ -0,0 +1,19 @@
+/*  https://leetcode.com/problems/build-array-from-permutation/(1920 leetcode)*/
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n];
+
+        for(int i = 0;i<n;i++)
+        {
+
+                arr[i]=nums[nums[i]];
+
+        }
+
+        return arr;
+
+
+    }
+}
